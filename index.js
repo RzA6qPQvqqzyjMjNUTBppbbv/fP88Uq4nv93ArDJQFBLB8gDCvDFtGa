5cd69cf53841d3f32e1ca3752aa8ac9a5118d096
RzA6qPQvqqzyjMjNUTBppbbv <97688473+RzA6qPQvqqzyjMjNUTBppbbv@users.noreply.github.com>
2022-06-25 19:54:52 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ document.getElementById("discord").textContent = `${json.discord}`;
 document.getElementById("nodejs").textContent = `${json.node}`;
 document.getElementById("uptime").textContent = `${json.uptime}`;
 })
+
+
+function reveal() {
+var reveals = document.querySelectorAll(".reveal");
+
+for (var i = 0; i < reveals.length; i++) {
+var windowHeight = window.innerHeight;
+var elementTop = reveals[i].getBoundingClientRect().top;
+var elementVisible = 150;
+
+if (elementTop < windowHeight - elementVisible) {
+  reveals[i].classList.add("active");
+} else {
+  reveals[i].classList.remove("active");
+}
+}
+}
+
+window.addEventListener("scroll", reveal);
+
